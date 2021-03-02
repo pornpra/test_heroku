66180f57263c55a3010c788b4e7ef0d1a98a5c94
pornpra <59501173+pornpra@users.noreply.github.com>
2021-03-02 14:36:23 +0700

    Add files via upload

--- predict_image.py
+++ predict_image.py
@@ -4,10 +4,11 @@ from botnoi import cv
 import numpy as np
 
 ### load model
-modFile = 'mymod.mod'
+modFile = 'mod_pimple.mod'
 mod = pickle.load(open(modFile,'rb'))
 def predicting(imgurl):
   a = cv.image(imgurl)
-  feat = a.getmobilenet()
+  feat = a.getresnet50()
+  #a.getmobilenet()
   res = mod.predict([feat])[0]
   return res
