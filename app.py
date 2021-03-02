1cfffcbba483f378b0c54dfd915bfb063af75e17
pornpra <59501173+pornpra@users.noreply.github.com>
2021-03-02 14:36:36 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -11,8 +11,20 @@ def index():
 def predict():
     p_image_url = request.values['p_image_url']
     res = pi.predicting(p_image_url)
+
+    if res == 0:
+        return 'สิวของคุณอยู่ในระดับเล็กน้อย (Level 0) คำแนะนำ คือ ล้างหน้าอย่างสม่ำเสมอและพักผ่อนให้เพียงพอ'
+    elif res == 1:
+        return 'สิวของคุณอยู่ในระดับปานกลาง (Level 1) คำแนะนำ คือ ใช้ยาแต้มสิว'
+    elif res == 2:
+        return 'สิวของคุณอยู่ในระดับรุนแรง (Level 2) คำแนะนำ คือ ควรรีบมาพบแพทย์เฉพาะทาง ติดต่อจองคิวได้ที่ 02-999-9999'
+    else:
+        return 'ส่งรูปภาพสิวของคุณมาใหม่อีกครั้ง'
+
     result = {'result':res}
-    return jsonify(result)
+    #jsonify(result)
+
+    return str(res)
 
 if __name__ == '__main__':
     app.run(debug=True)
